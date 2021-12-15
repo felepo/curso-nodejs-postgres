fd64e146bd52de672ffe06977ec796106ab64ca3
Fernando Poncio <fecrackle@gmail.com>
2021-12-15 00:14:11 -0600

    use sequelize ORM for the CRUD services

--- services/user.service.js
+++ services/user.service.js
@@ -6,27 +6,38 @@ class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+
+    return newUser;
   }
 
   async find() {
-    const rta = await models.User.findAll();
+    const users = await models.User.findAll();
+
+    return users;
+  }
+
+  async findOne(id) {
+    const user = await models.User.findByPk(id);
+
+    if(!user) {
+      throw boom.notFound('user not found');
+    }
+
+    return user;
+  }
+
+  async update(id, changes) {
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
 
     return rta;
   }
 
-  async findOne(id) {
-    return { id };
-  }
-
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
-
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
+
     return { id };
   }
 }
