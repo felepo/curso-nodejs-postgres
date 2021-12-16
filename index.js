58be92049ed28671434011136c0c2ac38aec9f45
Fernando Poncio <fecrackle@gmail.com>
2021-12-15 23:36:26 -0600

    add ORM handle error Middleware

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
-
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  ormErrorHandler,
+  boomErrorHandler
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -32,6 +36,7 @@ app.get('/nueva-ruta', (req, res) => {
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
